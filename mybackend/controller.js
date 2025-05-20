56b9bec63a40e6c325fbdd3cd7584ce83afdaa24
Kaveesha Subashana <kaveeshasubashana@gmail.com>
2025-05-21 01:51:03 +0530

    backend

---
+++ mybackend/controller.js
@@ -0,0 +1,22 @@
+const users = [
+
+    {
+        id:1,
+        name:'kavee',
+    },
+    {
+        id: 2,
+        name:'shakila',
+    },
+];
+
+const getUsers = (cb) => {
+    cb(users);
+};
+
+const getUserById = (id,cb)=>{
+    const user = users.find(user => user.id == id);
+    cb(user);
+};
+exports.getUsers = getUsers;
+exports.getUserById = getUserById;
